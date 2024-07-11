6c5ba8168b36a57207b55999469b73b8146d4dd7
Gerardo Urzua <gerardourzua20@gmail.com>
2024-07-11 13:31:53 -0700

    cors18

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,37 +1,45 @@
 module.exports = [
-  'strapi::errors',
-  'strapi::no-cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'https://minegocioenlinea-strapi.s3.us-east-2.amazonaws.com/',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://minegocioenlinea-strapi.s3.us-east-2.amazonaws.com/",
           ],
-          'media-src': [
+          "media-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'https://minegocioenlinea-strapi.s3.us-east-2.amazonaws.com/',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://minegocioenlinea-strapi.s3.us-east-2.amazonaws.com/",
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: "*", // Allows all origins. You can restrict it to specific origins if needed.
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+    },
+  },
 ];
